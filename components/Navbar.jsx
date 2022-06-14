62223c48169d21b2e30612f44ba60376b2ecdce9
theyashpatel <yashpatel26@gmail.com>
2022-06-14 16:04:55 +0530

    b

---
+++ components/Navbar.jsx
@@ -0,0 +1,66 @@
+import Link from "next/link";
+import React from "react";
+
+import {
+  FaBars,
+  FaTimes,
+  FaFacebook,
+  FaGithub,
+  FaTwitter,
+  FaLinkedin,
+} from "react-icons/fa";
+
+const Navbar = () => {
+  const links = [
+    {
+      id: 1,
+      link: "home",
+    },
+    {
+      id: 2,
+      link: "portfolio",
+    },
+    {
+      id: 3,
+      link: "experience",
+    },
+    {
+      id: 4,
+      link: "me",
+    },
+    {
+      id: 5,
+      link: "contact",
+    },
+  ];
+
+  return (
+    <div className="w-full h-20 z-10 fixed bg-white text-black duration-300 ease-in">
+      <div className="flex justify-between items-center w-full h-full max-w-screen-xl mx-auto p-4">
+        <Link href="/#home">
+          <h1 className="text-3xl lg:text-4xl font-bold uppercase underline underline-offset-2 tracking-wider cursor-pointer">
+            yash
+          </h1>
+        </Link>
+
+        <div>
+          <ul className="hidden md:flex">
+            {links.map(({ id, link }) => (
+              <Link key={id} href={`/#${link}`}>
+                <li className="ml-10 text-sm uppercase cursor-pointer duration-200 ease-out hover:scale-105 tracking-wider">
+                  {link}
+                </li>
+              </Link>
+            ))}
+          </ul>
+
+          <div className="md:hidden">
+            <FaBars size={30} />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
